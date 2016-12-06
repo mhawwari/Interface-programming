0c66f6d286e670faf29a5560da20781eff01685c
elsabergman <elsalbergman@yahoo.se>
2016-12-06 23:38:02 +0100

    Add files via upload

--- script_allbeverages.js
+++ script_allbeverages.js
@@ -28,36 +28,73 @@ success: function(main) {
     var last_name = main.payload[0].last_name;
      document.querySelector('.login_id').innerHTML = first_name +" "+ last_name;
     //document.querySelector('.login_id').innerHTML = last_name;
-    
+ ;
 }
         });
     });
 //console.log(history);
 //loop for all indices of array payload
+   var names = new Array();
+    var count = new Array();
+    var prices = new Array()
 $.each(allBeverages.payload, function(i, her)
 {
-$allBeverages.append('<div class="row">' +
+$allBeverages.append('<div class="row " id="'+her.beer_id+'">' + 
  			'<div class="col">'+ her.namn +'</div> ' +
  			'<div class="col">'+ her.count +'</div> ' +  	
- 			'<div class="col">'+ her.pub_price +'</div>' + '<input type="button" id="edit_bev" onclick="div_show()" class="dropbtn" value="EDIT">' + '</input>' + 
-			'</div> ' ); 
+ 			'<div class="col">'+ her.price +'</div>' + '<input type="button" id="edit_bev'+i+'" class="editbtn" value="EDIT">' + '</input>' + '</div>' ); 
+    
+
+   // should create form with loop!!! Call the function to update the database when press send. 
+      
+
+  
+    names += her.namn + ',';
+    count += her.count + ',';
+    prices += her.price + ',';
+    
+
+ 
   });
     
-   function getName() {
-       return her.namn;
-   }
-    function div_show() {
-        $('#popupEdit').show();
+    names = names.split(',');
+    count = count.split(',');
+    prices = prices.split(',');
+
+    $("#popupEdit").append('<form action="#" id="Editform" method="post" name="form">' +
+                            '<h2>Edit Beverages</h2>' +
+                            '<div id="input">' + '<input type="hidden" name="stock_hidden" id="stock_hidden" value="" />' + 
+                            '<input id="name_bev" name="name" value ="" type="text">' +  '<br>'
+                            + '<input id="stock" name="stock"  type="text">'
+                            +    '<input id="price" name="price" value = "price" type="text">' + '</div>' + 
+
+                            '<img id="close" src="http://www.freeiconspng.com/uploads/round-close-button-png-1.png" onclick="div_hide()">' + '<input type="submit"  class="dropbtn" id="submit_btn'+i+'" value="Send"/>' + '</form>');
+     
+    
+/*-- to make buttons work in a loop, with a different id to every EDIT button --*/
+    
+    function generate_handler(the_name, the_count, the_price ) {
+    return function(event) { 
         
+        div_show(the_name, the_count, the_price);
+    };
 }
-}
-});
+for(var i = 1; i <= allBeverages.payload.length; i++){
+    
+   $('#edit_bev'+i).click( generate_handler( names[i],count[i],prices[i]) );
     
     
+}
+
+     
+}
+
+ 
+});  
 });
 
-
-// Validating Empty Field
+    
+// Validating Empty Field, i ended up not using this so might remove it soon.
 function check_empty() {
 if (document.getElementById('name').value == "" || document.getElementById('stock').value == "" || document.getElementById('price').value == "") {
 alert("Fill All Fields !");
@@ -66,11 +103,36 @@ document.getElementById('Editform').submit();
 alert("Form Submitted");
 }
 }
-//Function To Display Popup
-function div_show() {
+//Function To Display Popup with pre filled values
+function div_show(name, count, price) {
+    $('#name_bev').val(name);
+    $('#stock').val(count);
+    $('#price').val(price);
+
+  
 document.getElementById('popupBody').style.display = "block";
+    
 }
 //Function to Hide Popup
 function div_hide(){
 document.getElementById('popupBody').style.display = "none";
 }
+/*---- this is work in progress to actually get the rows to update --*/
+function update_table(form) {
+    dive_hide();
+    
+       /* var name =  form.name.value;
+    
+    var stock = form.stock.value;
+    alert(stock);
+        document.getElementById('stock').innerHTML = stock;*/
+
+}
+    
+    /*window.onload = function() {
+    document.getElementById('Editform').onsubmit = function() {
+        
+    var txt = document.getElementById('stock');
+    txt.value = "updated " + txt.value;
+  };
+    }*/
